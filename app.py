6196d7f472cf5da5d30fcc1a7057ee9e37e1c7dc
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2020-12-23 14:57:35 +1100

    added check for negative

--- app.py
+++ app.py
@@ -11,12 +11,16 @@ def quadratic(a,b,c):
 
 a = st.slider("Value for a:", 1,5, 5)
 b = st.slider("Value for b:", -10, 10, -5)
-c = st.slider("Value for c:", -20,0, -5)
+c = st.slider("Value for c:", -20,10, -5)
 
 st.write("Quadratic equation in x:")
 st.latex(f"{a}x^2 + {b}x + {c} = 0")
 
-vals, (x_1, x_2) = quadratic(a,b,c)
+try:
+    vals, (x_1, x_2) = quadratic(a,b,c)
+except:
+    st.write("The discriminant is less than zero. Non-real solutions")
+    st.stop()
 #latex_code, vals = quadratic(a,b,c)
 st.latex(vals)
 st.write("Vals from returned dict:")
