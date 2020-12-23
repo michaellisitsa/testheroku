5d8e506f4a54c028b555aa1ab8d803b0b0545b3e
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2020-12-23 14:46:50 +1100

    Added roots together

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 def quadratic(a,b,c):
     x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
     x_2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
-    return locals()
+    return x_1, x_2
 
 a = st.slider("Value for a:", 1,5, 5)
 b = st.slider("Value for b:", -10, 10, -5)
@@ -16,9 +16,11 @@ c = st.slider("Value for c:", -20,0, -5)
 st.write("Quadratic equation in x:")
 st.latex(f"{a}x^2 + {b}x + {c} = 0")
 
-vals = quadratic(a,b,c)
+vals, (x_1, x_2) = quadratic(a,b,c)
 #latex_code, vals = quadratic(a,b,c)
-st.latex(vals[0])
+st.latex(vals)
 st.write("Vals from returned dict:")
 #st.write(vals[0])
-st.write("x_1:", vals[1]["x_1"], "x_2:", vals[1]["x_2"])
+st.write("x_1:", x_1, "x_2:", x_2)
+
+st.write(f"The two roots added together are: {x_1 + x_2}")
